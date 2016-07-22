bc72d07b30b5a88b494fe3ce89ae9ff809d76318
zhyingjia <965330394@qq.com>
2016-07-23 03:10:10 +0800

    first commit

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,271 @@
+describe ('formateTags test',function(){
+  it('foramteTags : ',function(){
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let result = formateTags(inputs);
+    expect(result).toEqual([{id:"ITEM0001",count:1},{id:"ITEM0013",count:2},{id:"ITEM0022",count:1}]);
+  });
+});
+
+describe ('matchId test',function(){
+  it('matchId : ',function(){
+    let inputs1 = [{id:"ITEM0001",count:1},{id:"ITEM0013",count:2},{id:"ITEM0022",count:1}];
+    let inputs2 = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00
+    }, {
+      id: 'ITEM0030',
+      name: '冰锋',
+      price: 2.00
+    }];
+
+    let result = matchId(inputs1,inputs2);
+    expect(result).toEqual(    [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1
+    }]);
+  });
+});
+
+describe ('noProSubTotal test',function(){
+  it('noProSubTotal : ',function(){
+    let inputs = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1
+    }]
+    let result = noProSubTotal(inputs);
+    expect(result).toEqual( [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1,
+      subTotal:18
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2,
+      subTotal:12
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1,
+      subTotal:8
+    }]);
+  });
+});
+
+describe ('total test',function(){
+  it('total : ',function(){
+    let inputs = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1,
+      subTotal:18
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2,
+      subTotal:12
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1,
+      subTotal:8
+    }];
+    let result = total(inputs);
+    expect(result).toEqual(38);
+  });
+});
+
+describe ('secondProTypeTotal test',function(){
+  it('secondProTypeTotal : ',function(){
+    let inputs1 =[{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1,
+      type:'指定菜品半价'
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2,
+      type:'-1'
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1,
+      type:'指定菜品半价'
+    }];
+      let inputs2 =  {
+        type: '指定菜品半价',
+        items: ['ITEM0001', 'ITEM0022']
+      };
+    let result = secondProTypeTotal(inputs1,inputs2);
+    expect(result).toEqual({
+      type: '指定菜品半价',
+      total : 25
+    });
+  });
+});
+
+
+describe ('firstProTypeTotal test',function(){
+  it('firstProTypeTotal : ',function(){
+    let inputs1 = 38;
+    let inputs2 = {
+      type: '满30减6元'
+    };
+    let result = firstProTypeTotal(inputs1,inputs2);
+    expect(result).toEqual({ type: '满30减6元',total : 32});
+  });
+});
+
+describe ('matchSecondProType test',function(){
+  it('matchSecondProType : ',function(){
+    let inputs1 =  [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1
+    }];
+
+    let inputs2 =  {
+      type: '指定菜品半价',
+      items: ['ITEM0001', 'ITEM0022']
+    };
+    let result = matchSecondProType(inputs1,inputs2);
+    expect(result).toEqual([{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1,
+      type:'指定菜品半价'
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2,
+      type:'-1'
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1,
+      type:'指定菜品半价'
+    }]);
+  });
+});
+
+
+describe ('getBestTotalType test',function(){
+  it('getBestTotalType : ',function(){
+
+    let inputs1 = {
+      type: '满30减6元',
+      total : 32
+    };
+    let inputs2 =  {
+      type: '指定菜品半价',
+      total : 25
+    };
+    let result = getBestTotalType(inputs1,inputs2);
+    expect(result).toEqual( {
+      type: '指定菜品半价',
+      total : 25
+    });
+  });
+});
+
+
+describe ('getProNames test',function(){
+  it('getProNames : ',function(){
+    let inputs =[{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00,
+      count:1,
+      type:'指定菜品半价'
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00,
+      count:2,
+      type:'-1'
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00,
+      count:1,
+      type:'指定菜品半价'
+    }];
+    let result = getProNames(inputs);
+    expect(result).toEqual(['黄焖鸡','凉皮']);
+  });
+});
+
+describe ('getPromotion test',function(){
+  it('getPromotion : ',function(){
+    let inputs1 = {
+      type: '指定菜品半价',
+      total : 25
+    };
+    let inputs2 = {
+      type : '-1',
+      total:30
+    }
+    let result = getPromotion(inputs1,inputs2);
+    expect(result).toEqual(5);
+  });
+});
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,126 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  let allItems = loadAllItems();
+  let allPromots = loadPromotions();
+  let formteTags = formteTags(selectedItems);
+  let cartItems = matchId(formteTags);
+  let noProSubItems  = noProSubTotal(cartItems);
+  let noProTotal = total(noProSubItems);
+  let firstProTotal = firstProTypeTotal(noProTotal,allPromots[0]);
+  let proCartItems = matchSecondProType(cartItems,allPromots[1]);
+  let secondProTotal = secondProTypeTotal(cartItems);
+  let bestProTotal =  getBestTotalType(firstProTotal, secondProTotal);
+  let proNames = getProNames(proCartItems);
+  let promotion = getPromotion(bestProTotal,noProTotal);
+console.log(promotion);
+  let src = '============= 订餐明细 =============\n';
+  for(let i=0;i<noProSubItems.length;i++){
+    src += noProSubItems[i].name + 'x' + noProSubItems[i].count +'=' +noProSubItems.subTotal + '元\n';
+
+  }
+  src += '-----------------------------------\n使用优惠:\n';
+  src +=bestProTotal.type+'(';
+  for(let i=0;i<proNames.length;i++){
+    src += proNames[i] + '，';
+  }
+  src +=')，省'+promotion + '元\n-----------------------------------\n总计：'+bestProTotal+'元';
+
+
+
+
+  return ;
 }
+function formateTags(userMenuTags){
+  return userMenuTags.map(function(tag){
+    let parsTag = tag.split("x");
+    return {
+      id : parsTag[0].trim(),
+      count : Number(parsTag[1].trim())
+    }
+  });
+}
+
+function matchId(formteTags,allItems){
+  let cartItems = [];
+  for(let i=0;i<formteTags.length; i++){
+    let exist = allItems.find(function(item){
+      return item.id ===formteTags[i].id;
+    });
+    if(exist){
+      cartItems.push(Object.assign({},exist,{count :formteTags[i].count}));
+    }
+  }
+ return cartItems;
+}
+ function noProSubTotal(cartItems){
+   return cartItems.map(function(item){
+     let subTotal = item.count * item.price;
+     return Object.assign(item,{subTotal : subTotal});
+   });
+ }
+
+function total(noProSubTotal){
+  let total = 0;
+  for(let i=0;i<noProSubTotal.length;i++){
+    total = Number(noProSubTotal[i].subTotal) + Number(total);
+  }
+  return total;
+}
+
+function firstProTypeTotal(noProSubTotal,firstProType){
+  let total = noProSubTotal > 30 ? (noProSubTotal-6) : noProSubTotal;
+  return total === noProSubTotal ? {type : '-1',total:total} : {type:firstProType.type, total:total};
+}
+
+function matchSecondProType(cartItems,secondProType){
+ let proCartItems = [];
+  for(let i=0;i<cartItems.length;i++){
+    let exist = secondProType.items.find(function(item){
+      return item === cartItems[i].id;
+    });
+    if(exist){
+      proCartItems.push(Object.assign({},cartItems[i],{type : secondProType.type}));
+    }else{
+      proCartItems.push(Object.assign({},cartItems[i],{type : "-1"}));
+    }
+  }
+  return proCartItems;
+}
+
+function secondProTypeTotal(proCartItems,secondProType){
+  let total = 0;
+  for(let i=0;i<proCartItems.length;i++){
+      if(proCartItems[i].type === "-1"){
+         total += proCartItems[i].count * proCartItems[i].price;
+      }else if (proCartItems[i].type === secondProType.type){
+        total += proCartItems[i].price * 0.5 *proCartItems[i].count;
+      }}
+  return {
+    type : secondProType.type,
+    total:total
+  };
+}
+
+function getBestTotalType(firstProTotal, secondProTotal){
+  return firstProTotal.total < secondProTotal.total ? firstProTotal : secondProTotal;
+}
+
+function getProNames(proCartItems){
+  let proNames = [];
+  for(let i=0;i<proCartItems.length;i++){
+
+    if(!(proCartItems[i].type === "-1")){
+        proNames.push(proCartItems[i].name);
+    }
+  }
+  return proNames;
+}
+
+function getPromotion(bestProTotal,noProTotal){
+
+  return Number(noProTotal.total) - Number(bestProTotal.total);
+}
+
+
+
+
+
