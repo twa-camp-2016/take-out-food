3e2e3e29ea608f80e7c752a881e7f3430bf47476
myc1995 <875940302@qq.com>
2016-07-23 15:09:12 +0800

    222

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -44,5 +44,79 @@ describe('Take out food', function () {
 ===================================`.trim()
     expect(summary).toEqual(expected)
   });
-
 });
+
+
+describe("formatTags",function () {
+  it("return right eachTagAmount",function () {
+    let selectedItems=["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let result=formatTags(selectedItems);
+    let subAmount=[{ id: 'ITEM0001 ', amount: 1 },{ id: 'ITEM0013 ', amount: 2 },{ id: 'ITEM0022 ', amount: 1 }]
+    expect(result).toEqual(subAmount);
+  });
+});
+
+describe("matchInformation",function () {
+  it("return right matchInformation",function () {
+    let allItems=loadAllItems();
+    let subAmount=[{id:'ITEM0001',amount:1},{id:'ITEM0013',amount:2},{id:'ITEM0022',amount:1}];
+    let subCount=[{ id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 }]
+    let result=matchInformation(subAmount,allItems)
+    expect(result).toEqual(subCount);
+  });
+});
+
+describe("calculateSubTotal",function () {
+  it("return right calculateSubTotal",function () {
+    let subCount=[{ id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 }]
+    let result=calculateSubTotal(subCount);
+    let subTotal=[{ id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 ,subTotal:18},
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 ,subTotal:12},
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 ,subTotal:8}]
+    expect(result).toEqual(subTotal);
+  })
+})
+
+describe("calculateTotal",function () {
+  it("return right total",function () {
+    let subTotal=[{ id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 ,subTotal:18},
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 ,subTotal:12},
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 ,subTotal:8}]
+    let result=calculateTotal(subTotal);
+    let total=38;
+    expect(result).toEqual(total);
+  })
+})
+
+describe("calculateSaves",function () {
+  it("return right saves",function () {
+    let promotions=[{
+      type: '满30减6元'
+    }, {
+      type: '指定菜品半价',
+      items: ['ITEM0001', 'ITEM0022']
+    }];
+    let subTotal=[{ id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 ,subTotal:18},
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 ,subTotal:12},
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 ,subTotal:8}]
+    let saves=[{ name: '肉夹馍', saves: 9 }, { name: '肉夹馍', saves: 4 }];
+    let result=calculateSaves(subTotal,promotions);
+    expect(result).toEqual(saves);
+  })
+})
+
+describe("selectBestTotal",function () {
+  it("return right bestTotal",function () {
+    let total=38;
+    let saves=13;
+    let bestTotal=[{ type: '指定菜品半价' ,save: 13 } ];
+    let result=selectBestTotal(total,saves);
+    expect(result).toEqual(bestTotal);
+  })
+})
+
+
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,121 @@
+let allItems=loadAllItems();
+let promotions=loadAllItems();
+
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  let subAmount= formatTags(selectedItems);
+  let subCount= matchInformation(subAmount,allItems);
+  let subTotal= calculateSubTotal(subCount);
+  let total=calculateTotal(subTotal);
+  let saves=calculateSaves(subTotal,promotions);
+  let bestTotal= selectBestTotal(total,saves);
+  return print(bestTotal,saves,subTotal,total);
+
+}
+
+function formatTags(selectedItems) {
+  let subAmount=[];
+  for(let i=0;i<selectedItems.length;i++){
+    let arr=selectedItems[i].split("x");
+    subAmount.push(Object.assign({},{id:arr[0]},{amount:parseInt(arr[1])}));
+  }
+  console.log(subAmount)
+  return subAmount;
+}
+
+function matchInformation(subAmount,allItems) {
+  let subCount=[];
+  for(let i=0;i<subAmount.length;i++){
+    for(let j=0;j<allItems.length;j++){
+      if(subAmount[i].id===allItems[j].id)
+        subCount.push(Object.assign({},allItems[j],{count:subAmount[i].amount}))
+    }
+  }
+  console.log(subCount)
+  return subCount;
+}
+
+function calculateSubTotal(subCount) {
+  let subTotal=[];
+  for(let i=0;i<subCount.length;i++){
+    let sum=0;
+    sum=subCount[i].price*subCount[i].count;
+    subTotal.push(Object.assign({},subCount[i],{subTotal:sum}));
+  }
+  console.log(subTotal);
+  return subTotal;
+}
+
+function calculateTotal(subTotal) {
+  let total=0;
+  for(let i=0;i<subTotal.length;i++){
+    total+=subTotal[i].subTotal;
+  }
+  console.log(total)
+  return total;
+}
+
+function calculateSaves(subTotal,promotions) {
+  let saves=[];
+  let save=0;
+  for(let i=0;i<subTotal.length;i++){
+    for(let j=0;j<promotions[1].items.length;j++){
+      if(subTotal[i].id===promotions[1].items[j]){
+        save=(subTotal[i].subTotal)/2;
+        saves.push(Object.assign({},{name:subTotal[1].name},{saves:save}))
+      }
+    }
+  }
+  console.log(saves)
+  return saves;
+}
+
+function selectBestTotal(total,saves) {
+  let bestTotal=[];
+  if(total>30 && saves===6){
+    bestTotal.push(Object.assign({},{type: '满30减6元'},{save:6}))
+    console.log(bestTotal);
+    return bestTotal;
+  }
+  else if(total>30 && saves>6){
+    bestTotal.push(Object.assign({},{type: '指定菜品半价'},{save:saves}))
+    console.log(bestTotal);
+    return bestTotal;
+  }
+  else if(total<30 && saves===0){
+    console.log(bestTotal);
+    return bestTotal;
+  }
+  else{
+    bestTotal.push(Object.assign({},{type: 'null'},{save:-1}))
+  }
+}
+
+function print(bestTotal,saves,subTotal,total) {
+  let result='';
+  console.log(bestTotal);
+  result+="============== 订餐明细 ============="+"\n";
+  for(let i=0;i<subTotal.length;i++){
+    result+=(subTotal[i].name+"x"+subTotal[i].count+"="+subTotal[i].subTotal+"元"+"\n");
+  }
+  result+="-----------------------------------"+"\n";
+  if(bestTotal[0].type!='满30减6元' && bestTotal[0].type!='指定菜品半价'){
+    result+=("总计"+total+"元\n"+" ===================================")
+  }
+  else if(bestTotal[0].type==='满30减6元'){
+    result+="使用优惠:"+"\n"+
+    "满30减6元，省6元"+"\n"+
+    "-----------------------------------"+"\n"+
+      "总计："+total-6+"元\n"+"===================================";
+  }
+  else if(bestTotal[0].type==='指定菜品半价'){
+    result+="使用优惠:"+"\n"+
+            "指定菜品半价(";
+    for(let j=0;j<saves.length;j++){
+      result+=saves[j].name+"，";
+    }
+    result+=")，省"+bestTotal[0].save+"元 "+"\n"+
+      "-----------------------------------"+"\n"+
+      "总计："+bestTotal[0].save+"元\n"+"===================================";
+  }
+  return result.trim();
 }
