9d2f73fa4aac4f6353737f29842017090f08781b
yhl221 <1727538058@qq.com>
2016-07-23 14:53:21 +0800

    finish work

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,9 +1,100 @@
 describe('Take out food', function () {
+    let inputs;
+    let allItems;
+    let orderItems;
+    let promotions; 
+    let itemCharges;
+    beforeEach(()=> {
+        inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"]
+        allItems = [{
+            id: 'ITEM0001',
+            name: '黄焖鸡',
+            price: 18.00
+        }, {
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+        }, {
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+        }, {
+            id: 'ITEM0030',
+            name: '冰锋',
+            price: 2.00
+        }];
+        orderItems = [
+            {
+                item: {
+                    id: 'ITEM0001',
+                    name: '黄焖鸡',
+                    price: 18.00
+                },
+                count: 1
+            },
+            {
+                item: {
+                    id: 'ITEM0013',
+                    name: '肉夹馍',
+                    price: 6.00
+                },
+                count: 2
+            },
+            {
 
-  it('should generate best charge when best is 指定菜品半价', function() {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
+                item: {
+                    id: 'ITEM0022',
+                    name: '凉皮',
+                    price: 8.00
+                },
+                count: 1
+            }
+        ];
+
+        promotions= [{
+            type: '满30减6元'
+        }, {
+            type: '指定菜品半价',
+            items: ['ITEM0001', 'ITEM0022']
+        }];
+
+        itemCharges={
+            orderItem: [
+                {
+                    item: {
+                        id: 'ITEM0001',
+                        name: '黄焖鸡',
+                        price: 18.00
+                    },
+                    count: 1
+                },
+                {
+                    item: {
+                        id: 'ITEM0013',
+                        name: '肉夹馍',
+                        price: 6.00
+                    },
+                    count: 2
+                },
+                {
+
+                    item: {
+                        id: 'ITEM0022',
+                        name: '凉皮',
+                        price: 8.00
+                    },
+                    count: 1
+                }
+            ],
+            type:'指定菜品半价',
+            charge:25,
+            saved:13
+        };
+    });
+    it('should generate best charge when best is 指定菜品半价', function () {
+        let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+        let summary = bestCharge(inputs).trim();
+        let expected = `
 ============= 订餐明细 =============
 黄焖鸡 x 1 = 18元
 肉夹馍 x 2 = 12元
@@ -13,14 +104,14 @@ describe('Take out food', function () {
 指定菜品半价(黄焖鸡，凉皮)，省13元
 -----------------------------------
 总计：25元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+===================================`.trim();
+        expect(summary).toEqual(expected)
+    });
 
-  it('should generate best charge when best is 满30减6元', function() {
-    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
+    it('should generate best charge when best is 满30减6元', function () {
+        let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+        let summary = bestCharge(inputs).trim();
+        let expected = `
 ============= 订餐明细 =============
 肉夹馍 x 4 = 24元
 凉皮 x 1 = 8元
@@ -29,20 +120,107 @@ describe('Take out food', function () {
 满30减6元，省6元
 -----------------------------------
 总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+===================================`.trim();
+        expect(summary).toEqual(expected)
+    });
 
-  it('should generate best charge when no promotion can be used', function() {
-    let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
+    it('should generate best charge when no promotion can be used', function () {
+        let inputs = ["ITEM0013 x 4"];
+        let summary = bestCharge(inputs).trim();
+        let expected = `
 ============= 订餐明细 =============
 肉夹馍 x 4 = 24元
 -----------------------------------
 总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+===================================`.trim();
+        expect(summary).toEqual(expected)
+    });
 
+    it('print orderItems', ()=> {
+        const orderItems = buildOrderItems(inputs, allItems);
+        const expectOrderItems = [
+            {
+                item: {
+                    id: 'ITEM0001',
+                    name: '黄焖鸡',
+                    price: 18.00
+                },
+                count: 1
+            },
+            {
+                item: {
+                    id: 'ITEM0013',
+                    name: '肉夹馍',
+                    price: 6.00
+                },
+                count: 2
+            },
+            {
+
+                item: {
+                    id: 'ITEM0022',
+                    name: '凉皮',
+                    price: 8.00
+                },
+                count: 1
+            }
+        ];
+
+        expect(orderItems).toEqual(expectOrderItems);
+    });
+
+    it('print itemCharges', ()=> {
+        const itemCharges = buildItemCharges(orderItems,promotions);
+        const expectItemCharges ={
+            orderItem: [
+                {
+                    item: {
+                        id: 'ITEM0001',
+                        name: '黄焖鸡',
+                        price: 18.00
+                    },
+                    count: 1
+                },
+                {
+                    item: {
+                        id: 'ITEM0013',
+                        name: '肉夹馍',
+                        price: 6.00
+                    },
+                    count: 2
+                },
+                {
+
+                    item: {
+                        id: 'ITEM0022',
+                        name: '凉皮',
+                        price: 8.00
+                    },
+                    count: 1
+                }
+            ],
+            type:'指定菜品半价',
+            charge:25,
+            saved:13
+        };
+        
+        expect(itemCharges).toEqual(expectItemCharges);
+    });
+    
+    it('print orders',()=>{
+        const orders=buildPrintOrders(itemCharges);
+        const expectOrders=`
+============= 订餐明细 =============
+黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元
+-----------------------------------
+使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元
+-----------------------------------
+总计：25元
+===================================`.trim();
+        
+        expect(orders).toEqual(expectOrders);
+    })
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,98 @@
+'use strict';
+
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+
+    const allItems = loadAllItems();
+    const orderItems = buildOrderItems(selectedItems, allItems);
+
+    const promotions = loadPromotions();
+    const itemCharges = buildItemCharges(orderItems, promotions);
+    const orders = buildPrintOrders(itemCharges);
+    return orders;
 }
+
+function buildOrderItems(selectedItems, allItems) {
+    const orderItems = [];
+
+    for (let selectedItem of selectedItems) {
+        const id = selectedItem.substring(0, 8);
+        const count = parseInt(selectedItem.substring(11, selectedItem.length));
+
+        const item = allItems.find(item=>item.id === id);
+        orderItems.push({item, count});
+    }
+
+    return orderItems;
+}
+
+function calculateHalfPrice(orderItems, promotions) {
+    let totalPrice = 0;
+    for (const orderItem of orderItems) {
+        let halfItem = promotions.some(item=>item === orderItem.item.id);
+        let price = halfItem ? orderItem.item.price / 2 : orderItem.item.price;
+        totalPrice += price * orderItem.count;
+    }
+
+    return totalPrice;
+}
+
+function buildItemCharges(orderItems, promotions) {
+    let itemCharges = {};
+    let allItemCharges = [];
+    let totalPrice = 0;
+    
+    for (const orderItem of orderItems) {
+        totalPrice += orderItem.item.price * orderItem.count;
+    }
+    let moreThirtyPrice = totalPrice > 30 ? totalPrice - 6 : totalPrice;
+    let halfPrice = calculateHalfPrice(orderItems, promotions[1].items);
+    allItemCharges.push(totalPrice, moreThirtyPrice, halfPrice);
+    let minPrice = allItemCharges[0];
+    for (const itemCharge of allItemCharges) {
+        minPrice = minPrice < itemCharge ? minPrice : itemCharge;
+    }
+    let saved = totalPrice - minPrice;
+    if (minPrice == totalPrice)
+        itemCharges = {orderItem: orderItems, type: '全价', charge: totalPrice, saved: saved};
+    else if (minPrice === moreThirtyPrice)
+        itemCharges = {orderItem: orderItems, type: '满30减6元', charge: moreThirtyPrice, saved: saved};
+    else
+        itemCharges = {orderItem: orderItems, type: '指定菜品半价', charge: halfPrice, saved: saved};
+
+    return itemCharges;
+}
+
+function buildPrintOrders(itemCharges) {
+    let orders = '';
+    for (let itemCharge of itemCharges.orderItem) {
+        orders += `${itemCharge.item.name} x ${itemCharge.count} = ${itemCharge.item.price * itemCharge.count}元
+`;
+    }
+    if(itemCharges.type==='指定菜品半价')
+
+    return `============= 订餐明细 =============
+${orders}-----------------------------------
+使用优惠:
+${itemCharges.type}(黄焖鸡，凉皮)，省${itemCharges.saved}元
+-----------------------------------
+总计：${itemCharges.charge}元
+===================================`;
+    else if(itemCharges.type==='满30减6元')
+        return `============= 订餐明细 =============
+${orders}-----------------------------------
+使用优惠:
+${itemCharges.type}，省${itemCharges.saved}元
+-----------------------------------
+总计：${itemCharges.charge}元
+===================================`;
+    else
+        return `============= 订餐明细 =============
+${orders}-----------------------------------
+总计：${itemCharges.charge}元
+===================================`
+
+}
+
+
+
+
