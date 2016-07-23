13a990e7dcbeac4d5073d534073a7c0e98336d31
Yvette <libra1014tyb@163.com>
2016-07-23 11:00:02 +0800

    10:59

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,5 +1,56 @@
 describe('Take out food', function () {
 
+  it('should count the tags',function () {
+    let tags = ['ITEM0001 x 1','ITEM0013 x 2'];
+    let countedIds = countIds(tags);
+    let expected=[
+      {
+        id:'ITEM0001',
+        count:1
+      },
+      {
+        id:'ITEM0013',
+        count:2
+      }
+    ];
+
+    expect(countedIds).toEqual(expected);
+  });
+
+  it('should build cart items',function () {
+    let countedIds=[
+      {
+        id:'ITEM0001',
+        count:1
+      },
+      {
+        id:'ITEM0013',
+        count:2
+      }
+    ];
+
+    let allItems=loadAllItems();
+
+    let cartItems=buildCartItems(countedIds,allItems);
+    let expected = [
+      {
+        id:'ITEM0001',
+        name:'黄焖鸡',
+        price:18.00,
+        count:1
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2
+      }
+    ];
+
+    expect(cartItems).toEqual(expected);
+  });
+
+
   it('should generate best charge when best is 指定菜品半价', function() {
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     let summary = bestCharge(inputs).trim();
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,23 @@
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+
+function countIds(tags) {
+  return tags.map((tag) => {
+    let [id,count] = tag.split(' x ');
+    return {id,count:parseFloat(count)};
+    });
+}
+
+function _getExistElementByIds(array, id) {
+  return array.find((element) => element.id===id);
+}
+
+function buildCartItems(countedIds,allItems) {
+  return countedIds.map(({id,count}) => {
+    let {name,price} = _getExistElementByIds(allItems,id);
+    return {id,name,price,count};
+    })
+}
+
+
