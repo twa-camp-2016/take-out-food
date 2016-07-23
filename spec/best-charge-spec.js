e701e3ca7eb07427641edeb78814104d80831a66
ZhuoYueYue <2574376092@qq.com>
2016-07-23 15:02:16 +0800

    zhuoyue

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,5 +1,135 @@
 describe('Take out food', function () {
 
+  it('should formatTags', function() {
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let expected =[ { id: 'ITEM0001', count: 1 },
+      { id: 'ITEM0013', count: 2 },
+      { id: 'ITEM0022', count: 1 } ];
+    expect(formatTags(inputs)).toEqual(expected)
+  });
+
+  it('should build Cart Items', function() {
+    let inputs = [ { id: 'ITEM0001', count: 1 },
+      { id: 'ITEM0013', count: 2 },
+      { id: 'ITEM0022', count: 1 } ];
+
+    allItems=[{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00
+    }, {
+      id: 'ITEM0030',
+      name: '冰锋',
+      price: 2.00
+    }];
+    let expected =[ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 } ];
+    expect(buildCartItems(inputs,allItems)).toEqual(expected)
+  });
+
+
+  it('it should build  Promoted  Items', function() {
+    let inputs = [{id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2},
+      {id: 'ITEM0022', name: '凉皮', price: 8, count: 1}];
+    let promotions = [{
+      type: '满30减6元'
+    }, {
+      type: '指定菜品半价',
+      items: ['ITEM0001', 'ITEM0022']
+    }];
+
+    let expected =[ { id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18,
+      count: 1,
+      halfSaved: 9,
+      nolmalPrice: 18 },
+      { id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6,
+        count: 2,
+        halfSaved: 0,
+        nolmalPrice: 12 },
+      { id: 'ITEM0022',
+        name: '凉皮',
+        price: 8,
+        count: 1,
+        halfSaved: 4,
+        nolmalPrice: 8 } ];
+    expect(buildPromotedItems(inputs,promotions)).toEqual(expected)
+  });
+
+
+
+
+  it('should calculate 没有优惠过的总价和半价商品一共优惠了多少钱', function() {
+    let inputs =[ { id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18,
+      count: 1,
+      halfSaved: 9,
+      nolmalPrice: 18 },
+      { id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6,
+        count: 2,
+        halfSaved: 0,
+        nolmalPrice: 12 },
+      { id: 'ITEM0022',
+        name: '凉皮',
+        price: 8,
+        count: 1,
+        halfSaved: 4,
+        nolmalPrice: 8 } ] ;
+    expected={ totalHalfSaved: 13, totalnoPromotedPrice: 38 };
+    expect(calculateTotalPrices(inputs)).toEqual(expected)
+  });
+
+  it('should build Receipt', function() {
+    let inputs =[ { id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18,
+      count: 1,
+      halfSaved: 9,
+      nolmalPrice: 18 },
+      { id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6,
+        count: 2,
+        halfSaved: 0,
+        nolmalPrice: 12 },
+      { id: 'ITEM0022',
+        name: '凉皮',
+        price: 8,
+        count: 1,
+        halfSaved: 4,
+        nolmalPrice: 8 } ] ;
+    calculatedTotalPrices={ totalHalfSaved: 13, totalnoPromotedPrice: 38 };
+    expected={ promotedItems:
+      [ { name: '黄焖鸡', price: 18, count: 1, nolmalPrice: 18, halfSaved: 9 },
+        { name: '肉夹馍', price: 6, count: 2, nolmalPrice: 12, halfSaved: 0 },
+        { name: '凉皮', price: 8, count: 1, nolmalPrice: 8, halfSaved: 4 } ],
+      savedItems: [ { name: '黄焖鸡' }, { name: '凉皮' } ],
+      fullDiscountPayPrice: 32,
+      halfDiscountPayPrice: 25,
+      totalHalfSaved: 13,
+      totalDiscountSaved: 6 };
+   let result=buildReceipt(inputs,calculatedTotalPrices);
+    expect(result).toEqual(expected)
+  });
+
+
+
   it('should generate best charge when best is 指定菜品半价', function() {
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     let summary = bestCharge(inputs).trim();
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,163 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+
+
+function formatTags(tags) {
+  return tags.map((tag) => {
+    let [id, count] = tag.split(' x ');
+    return {
+      id: id,
+      count: parseInt(count)
+    }
+  })
+}
+
+
+function fixPrice(number) {
+  return parseFloat(number.toFixed(2));
+}
+
+function getExistIdFromArray(id, array) {
+  for (let Item of array) {
+    if (Item.id === id) {
+      return Item;
+    }
+  }
+  return null;
+}
+
+function buildCartItems(formatTags, allItems) {
+  let result = [];
+  for (let formatTag of formatTags) {
+    let item = getExistIdFromArray(formatTag.id, allItems);
+    let cartItem = {
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      count: formatTag.count
+    }
+    result.push(cartItem);
+  }
+  return result;
+}
+
+function buildPromotedItems(cartItems, promotions) {
+  let currentPromotion = promotions.find((promotion) => {
+    return promotion.type === '指定菜品半价';
+  })
+  return cartItems.map((cartItem) => {
+    let hasPromoted = currentPromotion.items.includes(cartItem.id);
+    let nolmalPrice = cartItem.price * cartItem.count;
+    let halfSaved = hasPromoted ? nolmalPrice / 2 : 0;
+    return Object.assign({}, cartItem, {halfSaved: fixPrice(halfSaved), nolmalPrice});
+  })
+    ;
+}
+
+function calculateTotalPrices(promotedItems) {
+  return promotedItems.reduce((result, promotedItem) => {
+      result.totalHalfSaved += promotedItem.halfSaved;
+      result.totalnoPromotedPrice += promotedItem.nolmalPrice;
+      return result;
+    },
+    {
+      totalHalfSaved: 0, totalnoPromotedPrice: 0
+    }
+  )
+}
+
+function buildReceipt(promotedItems, {totalHalfSaved, totalnoPromotedPrice}) {
+
+  let halfsavedItems = promotedItems.filter((promotedItem) => {
+      return promotedItem.halfSaved > 0;
+    })
+    ;
+
+  let savedItems = halfsavedItems.map((halfsavedItem) => {
+      return {
+        name: halfsavedItem.name,
+      }
+    }
+  )
+  let fullDiscountPayPrice = totalnoPromotedPrice > 30 ? totalnoPromotedPrice - 6 : totalnoPromotedPrice;
+  let totalDiscountSaved = totalnoPromotedPrice > 30 ? 6 : 0;
+  let halfDiscountPayPrice = totalnoPromotedPrice - totalHalfSaved;
+  return {
+    promotedItems: promotedItems.map(({name, price, count, nolmalPrice, halfSaved}) => {
+        return {name, price, count, nolmalPrice, halfSaved}
+      }
+    ),
+    savedItems,
+    fullDiscountPayPrice,
+    halfDiscountPayPrice,
+    totalHalfSaved,
+    totalDiscountSaved
+
+  }
+}
+
+
+function bulidReceiptString(receipt) {
+  let line;
+  itemLine = [];
+  let lines = [`============= 订餐明细 =============`];
+  for (let item of receipt.promotedItems) {
+    line = `${item.name} x ${item.count} = ${item.price*item.count} 元`;
+    lines.push(line);
+  };
+
+  line = `-----------------------------------`;
+  lines.push(line)
+
+  if (receipt.totalHalfSaved > 0 || receipt.totalDiscountSaved > 0) {
+    line += `使用优惠:`;
+    if (receipt.fullDiscountPayPrice < receipt.halfDiscountPayPrice) {
+      line = `满30减6元，省6元`;
+      lines.push(line);
+      line = `-----------------------------------`;
+      lines.push(line);
+      line = `总计：${receipt.fullDiscountPayPrice}`;
+      lines.push(line);
+      line = `===================================`;
+      lines.push(line);
+      return lines.join('\n');
+    }
+
+    if (receipt.fullDiscountPayPrice > receipt.halfDiscountPayPrice) {
+      line += `指定菜品半价(`;
+      for (let item of receipt.savedItems) {
+        itemLine.push(item.name);
+      }
+      line += itemLine.join('，');
+      line += ')，';
+      line += `省${receipt.totalHalfSaved}元`
+      lines.push(line);
+      line = `-----------------------------------`;
+      lines.push(line);
+      line = `总计：${receipt.halfDiscountPayPrice}`;
+      lines.push(line);
+      line = `===================================`;
+      lines.push(line);
+      return lines.join('\n');
+    }
+  }
+  line = `总计：${receipt.fullDiscountPayPrice}`;
+  lines.push(line);
+  line = `===================================`;
+  lines.push(line);
+
+  return lines.join('\n');
+}
+
+function bestCharge(tags){
+  let tag = formatTags(tags);
+  let allItems = loadAllItems();
+  let cartItems = buildCartItems(tag, allItems);
+  let promotions = loadPromotions();
+  let promotedItems = buildPromotedItems(cartItems, promotions);
+  let calculatedTotalPrices = calculateTotalPrices(promotedItems);
+//console.log(promotedItems);
+  let builtReceipt = buildReceipt(promotedItems, calculatedTotalPrices);
+//console.log(builtReceipt);
+  let receiptString = bulidReceiptString(builtReceipt);
+  return  receiptString;
+
 }
