954b7dfd81c38111785bdd77b88a54f7e60a58dd
duanxue <1360132676@qq.com>
2016-07-23 14:35:51 +0800

    update -大教室——段雪

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,110 @@
+describe('formatTags',function(){
+  it('show barcodes',function(){
+    let tags= ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let barcodes=[{id:'ITEM0001',count:1},
+                  {id:'ITEM0013',count:2},
+                  {id:'ITEM0022',count:1}
+      ];
+    expect(formatTags(tags)).toEqual(barcodes);
+  });
+});
+
+describe('getOrders',function(){
+  it('show orders',function(){
+    let orders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1},
+                {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2},
+                {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1}];
+    let barcodes=[{id:'ITEM0001',count:1},
+      {id:'ITEM0013',count:2},
+      {id:'ITEM0022',count:1}
+    ];
+    expect(getOrders(barcodes,loadAllItems())).toEqual(orders);
+  })
+})
+
+describe('getSubtotal',function(){
+  it('show detailedOrders',function(){
+    let orders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1}];
+    let detailedOrders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,subtotal:18},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,subtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,subtotal:8}];
+    expect(getSubtotal(orders)).toEqual(detailedOrders);
+  })
+});
+
+describe('getTotal',function(){
+  it('show total',function(){
+    let detailedOrders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,subtotal:18},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,subtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,subtotal:8}];
+    expect(getTotal(detailedOrders)).toEqual(38);
+  });
+});
+
+describe('findPromotionTypes',function(){
+  it('show typeName',function(){
+    let detailedOrders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,subtotal:18},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,subtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,subtotal:8}];
+      let promotions=loadPromotions();
+      expect(findPromotionTypes(detailedOrders, promotions)).toEqual([{name:'黄焖鸡'},{name:'凉皮'}]);
+  })
+});
+
+describe('onePromotion',function(){
+  it('show promotionTotal',function(){
+    let detailedOrders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,subtotal:18},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,subtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,subtotal:8}];
+    let promotions=loadPromotions();
+    expect(onePromotion(detailedOrders,promotions)).toEqual([{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,promotionSubtotal:9},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,promotionSubtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,promotionSubtotal:4}]);
+  });
+});
+
+describe('findPromotion',function(){
+  it('show result',function(){
+    let total=38;
+    let promotionDetailedOrders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,promotionSubtotal:9},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,promotionSubtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,promotionSubtotal:4}];
+    let promotions=loadPromotions();
+    expect(findPromotion(promotionDetailedOrders,promotions,total)).toEqual({
+      type:'指定菜品半价',
+      promotionTotal:25
+    });
+  });
+});
+
+describe('print',function(){
+  it('show print',function(){
+    let promotionType={
+      type:'指定菜品半价',
+      promotionTotal:25
+    };
+    let detailedOrders=[{id: 'ITEM0001', name: '黄焖鸡', price: 18.00,count:1,subtotal:18},
+      {id: 'ITEM0013', name: '肉夹馍', price: 6.00,count:2,subtotal:12},
+      {id: 'ITEM0022',name: '凉皮', price: 8.00,count:1,subtotal:8}];
+    let total=38;
+    let typeName=[{name:'黄焖鸡'},{name:'凉皮'}];
+    let result=print(promotionType,detailedOrders,total,typeName).trim();
+    expect(result).toEqual(`
+============= 订餐明细 =============
+黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元
+-----------------------------------
+使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元
+-----------------------------------
+总计：25元
+===================================`.trim());
+  });
+});
+
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,123 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+'use strict'
+function bestCharge(tags) {
+  let items = loadAllItems();
+  let promotions = loadPromotions();
+  let barcodes = formatTags(tags);
+  let orders = getOrders(barcodes, items);
+  let detailedOrders = getSubtotal(orders);
+  let total = getTotal(detailedOrders);
+  let typeName = findPromotionTypes(detailedOrders, promotions);
+  let promotionDetailedOrders = onePromotion(detailedOrders, promotions);
+  let promotionType = findPromotion(promotionDetailedOrders, promotions, total);
+  let str = print(promotionType, detailedOrders, total, typeName);
+  return str;
 }
+function formatTags(tags) {
+  return tags.map(function (tag) {
+    let info = tag.split(' x ');
+    return {
+      id: info[0],
+      count: parseInt(info[1])
+    }
+  });
+}
+function getOrders(barcodes, items) {
+  return barcodes.map(function (bar) {
+    return Object.assign({}, items.find(function (item) {
+      return item.id === bar.id;
+    }), {count: bar.count});
+  });
+}
+function getSubtotal(orders) {
+  return orders.map(function (order) {
+    return Object.assign({}, order, {subtotal: order.price * order.count});
+  });
+}
+function getTotal(detailedOrders) {
+  let total = 0;
+  for (let order of detailedOrders) {
+    total += order.subtotal;
+  }
+  return total;
+}
+function findPromotionTypes(detailedOrders, promotions) {
+  let typeName = [];
+  for (let order of detailedOrders) {
+    let existItem = promotions[1].items.find(function (item) {
+      return item === order.id;
+    });
+    if (existItem) {
+      typeName.push({name: order.name});
+    }
+  }
+  return typeName;
+}
+function onePromotion(detailedOrders, promotions) {
+  let promotionDetailedOrders = [];
+  for (let order of detailedOrders) {
+    let existItem = promotions[1].items.find(function (item) {
+      return item === order.id;
+    });
+    if (existItem) {
+      promotionDetailedOrders.push(Object.assign({}, {
+        id: order.id,
+        name: order.name,
+        price: order.price,
+        count: order.count
+      }, {promotionSubtotal: order.subtotal / 2}));
+    }
+    else
+      promotionDetailedOrders.push(Object.assign({}, {
+        id: order.id,
+        name: order.name,
+        price: order.price,
+        count: order.count
+      }, {promotionSubtotal: order.subtotal}));
+  }
+  return promotionDetailedOrders;
+}
+function findPromotion(promotionDetailedOrders, promotions, total) {
+  let onePromotionTotal = 0;
+  if (total >= 30) {
+    onePromotionTotal = total - 6;
+  }
+  let anotherPromtoinTotal = 0;
+  for (let order of promotionDetailedOrders) {
+    anotherPromtoinTotal += order.promotionSubtotal;
+  }
+  if (onePromotionTotal <= anotherPromtoinTotal) {
+    return {
+      type: '满30减6元',
+      promotionTotal: onePromotionTotal
+    };
+  }
+  else
+    return {
+      type: '指定菜品半价',
+      promotionTotal: anotherPromtoinTotal
+    };
+}
+function print(promotionType, detailedOrders, total, typeName) {
+  let str = '============= 订餐明细 =============\n';
+  for (let order of detailedOrders) {
+    str += order.name + ' x ' + order.count + ' = ' + order.subtotal + '元\n';
+  }
+  str += '-----------------------------------\n';
+  if (promotionType.promotionTotal===0) {
+    str += '总计：' + total + '元\n===================================';
+  }
+  else {
+    str += '使用优惠:\n';
+    str += promotionType.type;
+    if (promotionType.type === '指定菜品半价') {
+      str += '(' + typeName[0].name;
+      for (let i = 1; i < typeName.length; i++) {
+        str += '，' + typeName[i].name + ')';
+      }
+    }
+    str += '，省' + (total - promotionType.promotionTotal) + '元\n';
+    str += '-----------------------------------\n总计：' + promotionType.promotionTotal + '元\n===================================';
+  }
+  return str;
+}
+bestCharge(tags);
