86ce3e1bf1ae6b2001fb53a1277277685ad8d7d3
YKKKY <1131818177@qq.com>
2016-07-23 14:44:50 +0800

    discount

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -45,4 +45,148 @@ describe('Take out food', function () {
     expect(summary).toEqual(expected)
   });
 
+  it(('cartItems'),()=>{
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+
+    const cartItems=[{item:{
+                    id: 'ITEM0001',
+                    name: '黄焖鸡',
+                    price: 18.00
+                      },count:1},
+
+                    { item:{
+                      id: 'ITEM0013',
+                      name: '肉夹馍',
+                      price: 6.00
+                    },count:2},
+                     {item:{
+                        id: 'ITEM0022',
+                        name: '凉皮',
+                        price: 8.00
+                        },count:1}
+
+    ];
+    expect(buildCartItems(inputs)).toEqual(cartItems);
+  })
+
+  const cartItems=[{item:{
+    id: 'ITEM0001',
+    name: '黄焖鸡',
+    price: 18.00
+  },count:1},
+
+    { item:{
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00
+    },count:2},
+    {item:{
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00
+    },count:1}];
+
+  it(('receiptItem'),()=>{
+
+    const receipItems=[{cartItem:{item:{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00
+    },count:1},
+    save:{discountname:'指定菜品半价',save:9},
+      subtotal:9},
+
+      {cartItem:{  item:{
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00
+        },count:2},
+        save:{discountname:'null',save:0},
+        subtotal:2},
+
+      {cartItem:{item:{
+      id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00
+    },count:1},
+        save:{discountname:'指定菜品半价',save:4},
+        subtotal:4
+      }
+
+    ];
+    expect(buildReceiptItems(cartItems)).toEqual(receipItems);
+  });
+  const receipItems=[{cartItem:{item:{
+    id: 'ITEM0001',
+    name: '黄焖鸡',
+    price: 18.00
+  },count:1},
+    save:{discountname:'指定菜品半价',save:9},
+    subtotal:9},
+
+    {cartItem:{item:{
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00
+    },count:2},
+      save:{discountname:'null',save:0},
+      subtotal:2},
+
+    {cartItem:{item:{
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00
+    },count:1},
+      save:{discountname:'指定菜品半价',save:4},
+      subtotal:4
+    }
+  ];
+
+  it((''),() =>{
+    const receipt=[{cartItem:{item:{
+                    id: 'ITEM0001',
+                    name: '黄焖鸡',
+                    price: 18.00
+                   },count:1},
+                    save:{discountname:'指定菜品半价',save:9},
+                     subtotal:9},
+
+      {cartItem:{item:{
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00
+      },count:2},
+        save:{discountname:'null',save:0},
+        subtotal:12},
+
+      {cartItem:{item:{
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00
+      },count:1},
+        save:{discountname:'指定菜品半价',save:4},
+        subtotal:4
+      }];
+      total:25,
+      save=[{saveWay:`指定菜品半价`,item},{save:13}];
+    expece(buildReceipt(receipItems)).toEqual(receipt);
+
+  });
+
+  it(('correct print'),()=>{
+
+    let expected = `
+============= 订餐明细 =============
+黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元
+-----------------------------------
+使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元
+-----------------------------------
+总计：25元
+===================================`
+    expect(buildReceiptText(receipt)).toEqual(text);
+  })
+
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,133 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+function bestCharge(inputs) {
+
+
+  const cartItems=buildCartItems(inputs,allItems);
+  const receiptItems=buildReceiptItems(receiptItems);
+  const receipt=buildReceipt(receiptItems);
+  const receiptText=buildReceiptText(receipt);
+
+  console.log(receiptText);
+
+ }
+
+function buildCartItems(inputs) {
+
+  const cartItems = [];
+  const allItems = loadAllItems();
+
+  for (const input of inputs) {
+
+    const id = input.slice(0, input.indexOf('x') - 1);
+    const count = parseInt(input.slice(-1));
+
+    let cartItem = cartItems.find(cartItem=>cartItem.item.id === id);
+    if (cartItem || !cartItems.length) {
+
+      const item = allItems.find(item=>item.id === id);
+      cartItems.push({item, count});
+    }
+  }
+  return cartItems;
 }
+
+function buildReceiptItems(cartItems) {
+
+  const promotions = loadPromotions();
+
+  return cartItems.map(cartItem=> {
+
+    const promotion = buildPromotionType(cartItem, promotions);
+    const {save,subtotal}=discount(cartItem.item.price, cartItem.count, promotions);
+
+    return {cartItem, save, subtotal};
+
+  })
+}
+
+function findPromotionType(id, promotions) {
+
+  const promotion = promotions.find(promotion=>promotion.items.some(b=>b === id));
+
+  if (promotion) {
+
+    return promotion ? promotion.type : undefined;
+  } else {
+    return null;
+  }
+
+}
+
+function discount(price, count, promotions) {
+
+  let subtotal = price * count;
+  let save = {discountname: 'null', savetoal: price};
+
+  if (promotions) {
+    save = {discountname: '指定菜品半价', savetoal: price / 2};
+  }
+
+  return {save, subtotal};
+}
+
+
+function getSaveItem(receiptItms) {
+
+  let saveItem = [];
+
+  for (const receipt of receiptItms) {
+
+    const existDiscount = receipt.cartItem;
+    if (existDiscount.save.discountname) {
+
+      saveItem.push(existDiscount.item.name);
+    }
+  }
+  return saveItem;
+}
+
+function buildReceipt(receiptItms) {
+
+  let subtotal = 0;
+  let save = 0;
+  let saveWay = [];
+  let saveItem=[];
+
+  for (const receipt of receiptItms) {
+
+    let subtotal = receiptItms.savetoal;
+    let save = receiptItms.save.savetoal;
+
+
+     saveItem.push( getSaveItem(receiptItms));
+
+    if (subtotal > 30 && (subtotal - 6) >= subtotal - save) {
+
+      saveWay.push({saveway: '指定菜品半价'}, {save: save})
+    } else if (subtotal > 30 && (subtotal - 6) <= subtotal - save) {
+
+      saveWay.push({saveway: '满30减6元'}, {save: save}, saveItem);
+    }
+  }
+  return {receiptItms, saveWay, subtotal, saveItem};
+
+}
+
+function buildReceiptText(receiptItems) {
+
+  let receiptText = receiptItems.map(receipt=> {
+
+    const cartItem = receipt.cartItem;
+    return `${cartItem.item.price} x${cartItem.count}=${cartItem.subtotal}`.join('\n');
+  })
+
+  return receiptText += `============= 订餐明细 =============
+${cartItem}
+-----------------------------------
+  使用优惠:
+${receipt.saveWay}${receipt.saveItem}
+-----------------------------------
+  总计：${receiptItems.subtotal}
+===================================    `
+
+}
+
