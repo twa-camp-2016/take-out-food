a9b48c971745ff3ed4627ac0842d05f99fc78b67
preservance717 <GL717@outlook.com>
2016-07-23 10:57:20 +0800

    add buldOrderItems

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,7 +1,20 @@
+'use strict';
+
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
 
-function buildOrderItems(inputs, allItems) {
-  
+function buildOrderItems(tags, allItems) {
+  let orderItems = [];
+
+  for (let tag of tags) {
+    const splittedTag = tag.split('x');
+    const id = splittedTag[0].slice(0, -1);
+    const count = parseInt(splittedTag[1]);
+
+    let item = allItems.find(item => item.id === id);
+    orderItems.push({item, count});
+  }
+
+  return orderItems;
 }
