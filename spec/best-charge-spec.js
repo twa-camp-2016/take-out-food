94932d6b36a1f1124433421ccd65142cc200f99f
yangyue <m18710434142_1@163.com>
2016-07-23 14:57:58 +0800

    first

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,4 +1,6 @@
 describe('Take out food', function () {
+  const allItems = loadAllItems();
+  const promotions = loadPromotions();
 
   it('should generate best charge when best is 指定菜品半价', function() {
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
@@ -13,9 +15,91 @@ describe('Take out food', function () {
 指定菜品半价(黄焖鸡，凉皮)，省13元
 -----------------------------------
 总计：25元
-===================================`.trim()
+===================================`.trim();
     expect(summary).toEqual(expected)
   });
+
+  it('指定菜品半价,should print correct cartItems', function() {
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+
+    const cartItems = buildCartItems(inputs,allItems);
+
+    let expected = [{
+      item:{
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00
+      },
+      count:1
+     },
+      {
+      item:{
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00
+      },
+      count :2
+      },
+      {
+        item: {
+          id: 'ITEM0022',
+          name: '凉皮',
+          price: 8.00
+        },
+        count: 1
+      }];
+    expect(cartItems).toEqual(expected)
+  });
+  it('指定菜品半价,should print correct promotionItems', function() {
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+
+    const cartItems = buildCartItems(inputs,allItems);
+    const promotionItems = buildPromotionItems(cartItems,promotions);
+    let expected = {
+      cartItems: [{
+        item: {
+          id: 'ITEM0001',
+          name: '黄焖鸡',
+          price: 18.00
+        },
+        count: 1
+      },
+        {
+          item: {
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+          },
+          count: 2
+        },
+        {
+          item: {
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+          },
+          count: 1
+        }],
+      savedPromotions:[{
+        promotion:
+        {
+          type: '满30减6元'
+        },
+        saved:6,
+        total:32
+      },
+        {
+         promotion:{
+           type: '指定菜品半价',
+           items: ['ITEM0001', 'ITEM0022']
+         },
+          saved: 13,
+          total:25
+        }]
+    };
+    expect(promotionItems).toEqual(expected)
+  });
+
 
   it('should generate best charge when best is 满30减6元', function() {
     let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
@@ -29,11 +113,35 @@ describe('Take out food', function () {
 满30减6元，省6元
 -----------------------------------
 总计：26元
-===================================`.trim()
+===================================`.trim();
     expect(summary).toEqual(expected)
   });
 
-  it('should generate best charge when no promotion can be used', function() {
+  it('满30减6元,should print correct cartItems', function() {
+    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+    const cartItems = buildCartItems(inputs,allItems);
+
+    let expected = [
+      {
+        item:{
+          id: 'ITEM0013',
+          name: '肉夹馍',
+          price: 6.00
+        },
+        count :4
+      },
+      {
+        item: {
+          id: 'ITEM0022',
+          name: '凉皮',
+          price: 8.00
+        },
+        count: 1
+      }];
+    expect(cartItems).toEqual(expected)
+  });
+
+ it('should generate best charge when no promotion can be used', function() {
     let inputs = ["ITEM0013 x 4"];
     let summary = bestCharge(inputs).trim();
     let expected = `
@@ -41,8 +149,26 @@ describe('Take out food', function () {
 肉夹馍 x 4 = 24元
 -----------------------------------
 总计：24元
-===================================`.trim()
+===================================`.trim();
     expect(summary).toEqual(expected)
   });
+
+  it('when no promotion can be used,should print correct cartItems', function() {
+    let inputs = ["ITEM0013 x 4"];
+    const cartItems = buildCartItems(inputs,allItems);
+
+    let expected = [
+      {
+        item:{
+          id: 'ITEM0013',
+          name: '肉夹馍',
+          price: 6.00
+        },
+        count :4
+      }
+     ];
+    expect(cartItems).toEqual(expected)
+  });
+
 
 });
