a16578bdbdd95c4178c90f267b6024b6b91a98a1
liuru123 <liuru@1457816105qq.com>
2016-07-23 15:14:21 +0800

    大教室-刘茹

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,87 @@
+'use strict';
+
+describe("formattedTags",function () {
+  it("it should return the array",function(){
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let result=formattedTags(inputs);
+    let expected=[ { id: 'ITEM0001', count: 1 },
+      { id: 'ITEM0013', count: 2 },
+      { id: 'ITEM0022', count: 1 } ];
+    expect(result).toEqual(expected);
+  });
+});
+describe("generateAboutItems",function () {
+  it("should return the merged inputs and items",function () {
+    let inputs=[ { id: 'ITEM0001', count: 1 },
+      { id: 'ITEM0013', count: 2 },
+      { id: 'ITEM0022', count: 1 } ];
+    let allItems=[ { id: 'ITEM0001', name: '黄焖鸡', price: 18 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6 },
+      { id: 'ITEM0022', name: '凉皮', price: 8 },
+      { id: 'ITEM0030', name: '冰锋', price: 2 } ];
+
+    let result=generateAboutItems(allItems,inputs);
+    let expected=[ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 } ];
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("computeSubtotal",function () {
+  it("should return the subtotal before the promotion ",function () {
+    let inputs=[ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1 } ];
+    let result=computeSubtotal(inputs);
+    let expected=[ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1, subtotal: 18 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2, subtotal: 12 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1, subtotal: 8 } ];
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("computeTotal",function () {
+  it("should return the total",function(){
+    let inputs=[ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1, subtotal: 18 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2, subtotal: 12 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1, subtotal: 8 } ];
+    let result=computeTotal(inputs);
+    let expected=38;
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("generatePromotions",function(){
+  it("should return the promotion items",function () {
+    let inputs=[ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1, subtotal: 18 },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2, subtotal: 12 },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: 1, subtotal: 8 } ];
+
+    let  inputs1= [ { type: '满30减6元' },
+      { type: '指定菜品半价', items: [ 'ITEM0001', 'ITEM0022' ] } ];
+    let result=generatePromotions(inputs,inputs1);
+
+    let expected=
+      [ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1, subtotal: 9 },
+        { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2, subtotal: 12 },
+        { id: 'ITEM0022', name: '凉皮', price: 8, count: 1, subtotal: 4 } ];
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("computeSavedTotal",function () {
+  it("return the savedTotal",function(){
+    let promotionItems=
+      [ { id: 'ITEM0001', name: '黄焖鸡', price: 18, count: 1, subtotal: 9 },
+        { id: 'ITEM0013', name: '肉夹馍', price: 6, count: 2, subtotal: 12 },
+        { id: 'ITEM0022', name: '凉皮', price: 8, count: 1, subtotal: 4 } ];
+    let result=computeSavedTotal(promotionItems);
+    let expected=25;
+    expect(result).toEqual(expected);
+  });
+});
+
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
@@ -44,5 +128,4 @@ describe('Take out food', function () {
 ===================================`.trim()
     expect(summary).toEqual(expected)
   });
-
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,135 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+'use strict';
+function formattedTags(inputs) {
+  return inputs.map(function (input) {
+    let temp = input.split(' x ');
+    return {id: temp[0], count: parseInt(temp[1])};
+});
 }
+
+
+function generateAboutItems(allItems, formattedInputs) {
+  let result = [];
+  for (let i = 0; i < formattedInputs.length; i++) {
+    let exist = allItems.find(function (item) {
+      return item.id === formattedInputs[i].id;
+    });
+    if (exist) {
+      result.push(Object.assign({}, exist, {count: formattedInputs[i].count}));
+    }
+  }
+  return result;
+}
+
+
+function computeSubtotal(generateItems) {
+  let result = [];
+  for (let i = 0; i < generateItems.length; i++) {
+    result.push(Object.assign({}, generateItems[i], {subtotal: generateItems[i].price * generateItems[i].count}));
+  }
+  return result;
+}
+function computeTotal(generateSubtotal) {
+  let sum = 0;
+  for (let i = 0; i < generateSubtotal.length; i++) {
+    sum += generateSubtotal[i].subtotal;
+  }
+  return sum;
+}
+
+
+function generatePromotions(generateSubtotal, promotionArray) {
+  let result = [];
+  for (let i = 0; i < generateSubtotal.length; i++) {
+    let exist = promotionArray[1].items.find(function (item) {
+      return item === generateSubtotal[i].id;
+    });
+    var flag = 0;
+    if (exist) {
+      flag = 1;
+      result.push(Object.assign({}, {
+        id: generateSubtotal[i].id,
+        name: generateSubtotal[i].name,
+        price: generateSubtotal[i].price,
+        count: generateSubtotal[i].count,
+        subtotal: parseFloat(generateSubtotal[i].subtotal / 2)
+      }));
+
+    }
+    if (flag === 0) {
+      result.push(Object.assign({}, generateSubtotal[i]));
+    }
+  }
+  return result;
+}
+
+function computeSavedTotal(promotionsItems) {
+  let sum = 0;
+  for (let i = 0; i < promotionsItems.length; i++) {
+    sum += promotionsItems[i].subtotal;
+  }
+  return sum;
+}
+
+
+
+function print(promotionsItems, total, savedTotal, generateSubtotal) {
+  let result = "============= 订餐明细 =============";
+  if (total === savedTotal) {
+
+    for (let i = 0; i < promotionsItems.length; i++) {
+      result += "\n" + promotionsItems[i].name + " x " +promotionsItems[i].count+ " = " + generateSubtotal[i].subtotal + "元";
+    }
+    result += "\n-----------------------------------";
+    result += "\n总计：" + total + "元";
+    result += "\n===================================";
+  }
+
+
+  else if (savedTotal === total - 6) {
+    for (let i = 0; i < promotionsItems.length; i++) {
+      result += "\n" + promotionsItems[i].name + " x " + promotionsItems[i].count + " = " + generateSubtotal[i].subtotal + "元";
+    }
+    result += "\n-----------------------------------";
+    result += "\n使用优惠:";
+    result += "\n满30减6元，省6元";
+    result += "\n-----------------------------------";
+    result += "\n总计：" + savedTotal;
+    result += "\n===================================";
+  }
+  else {
+    for (let i = 0; i < promotionsItems.length; i++) {
+      result += "\n" + promotionsItems[i].name + " x " + promotionsItems[i].count + " = " + generateSubtotal[i].subtotal + "元";
+    }
+      result += "\n-----------------------------------";
+      result += "\n使用优惠:";
+      result += "\n指定菜品半价(";
+      for (let j = 0; j < generateSubtotal.length; j++) {
+        if (generateSubtotal[j].subtotal === 2 * promotionsItems[j].subtotal) {
+          result += generateSubtotal[j].name;
+          result += "，";
+        }
+      }
+      result = result.substr(0, result.length - 1);
+      result += ")，";
+      result += "省" + (total-savedTotal) + "元";
+      result += "\n-----------------------------------";
+      result += "\n总计：" + savedTotal+"元";
+      result += "\n===================================";
+    }
+    return result;
+}
+
+function bestCharge(inputs) {
+
+  let allItems = loadAllItems();
+  let promotionArray = loadPromotions();
+  let formattedInputs = formattedTags(inputs);
+  let generateItems = generateAboutItems(allItems, formattedInputs);
+  let generateSubtotal = computeSubtotal(generateItems);
+  let total = computeTotal(generateSubtotal);
+  let promotionsItems = generatePromotions(generateSubtotal, promotionArray, total);
+  let savedTotal = computeSavedTotal(promotionsItems);
+  let result= print(promotionsItems, total, savedTotal, generateSubtotal);
+  return result;
+}
+bestCharge(inputs);
