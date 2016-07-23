c3e811b21e22533aa2e9128c24da2e2a35a21ff4
Miss-qi <1370322806@qq.com>
2016-07-23 15:00:33 +0800

    first commit

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,321 @@
+describe("amountId",function () {
+  it("amountId test",function () {
+    let selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let result = amountId(selectedItems);
+    let itemsTag = [
+      {
+        id:'ITEM0001',
+        amount:1
+      },
+      {
+        id:'ITEM0013',
+        amount:2
+      },
+      {
+        id:'ITEM0022',
+        amount:1
+      }
+    ];
+    expect(result).toEqual(itemsTag);
+  })
+});
+
+describe("matchPromotions",function () {
+  it("matchPromotions test",function () {
+    let itemsIdCount = [
+      {
+        id:'ITEM0001',
+        amount:1
+      },
+      {
+        id:'ITEM0013',
+        amount:2
+      },
+      {
+        id:'ITEM0022',
+        amount:1
+      }
+    ];
+    let promotionsList = [{
+      type: '满30减6元'
+      },
+      {
+      type: '指定菜品半价',
+      items: ['ITEM0001', 'ITEM0022']
+      }];
+    let result = matchPromotions(itemsIdCount,promotionsList);
+    let expected = [
+      {
+        id:'ITEM0001',
+        amount:1,
+        type: '指定菜品半价'
+      },
+      {
+        id:'ITEM0013',
+        amount:2,
+        type: '满30减6元'
+      },
+      {
+        id:'ITEM0022',
+        amount:1,
+        type: '指定菜品半价'
+      }
+    ];
+    expect(result).toEqual(expected);
+  })
+});
+
+describe("matchItems",function () {
+  it("matchItems test",function () {
+    let itemsPromotionList = [
+      {
+        id:'ITEM0001',
+        amount:1,
+        type: '指定菜品半价'
+      },
+      {
+        id:'ITEM0013',
+        amount:2,
+        type: '满30减6元'
+      },
+      {
+        id:'ITEM0022',
+        amount:1,
+        type: '指定菜品半价'
+      }
+    ];
+    let allItems = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00
+      },
+      {
+        id: 'ITEM0030',
+        name: '冰锋',
+        price: 2.00
+      }];
+    let result = matchItems(itemsPromotionList, allItems);
+    let expected = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价'
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元'
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价'
+      }
+    ];
+    expect(result).toEqual(expected);
+  })
+});
+
+describe("calculateSubtotal",function () {
+  it("calculateSubtotal test",function () {
+    let itemsList = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价'
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元'
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价'
+      }
+    ];
+    let result = calculateSubtotal(itemsList);
+    let expected = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价',
+        subtotal:18.00
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元',
+        subtotal:12.00
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价',
+        subtotal:8.00
+      }
+    ];
+    expect(result).toEqual(expected);
+  })
+});
+
+describe("calculateTotal",function () {
+  it("calculateTotal test",function () {
+    let itemSubtotal = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价',
+        subtotal:18.00
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元',
+        subtotal:12.00
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价',
+        subtotal:8.00
+      }
+    ];
+    let result = calculateTotal(itemSubtotal);
+    let expected = 38.00;
+    expect(result).toEqual(expected);
+  })
+});
+
+describe("calculateSavedSubtotal",function () {
+  it("calculateSavedSubtotal test",function () {
+    let itemsList = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价'
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元'
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价'
+      }
+    ];
+    let result = calculateSavedSubtotal(itemsList);
+    let expected = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价',
+        discountSubtotal:9.00
+
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元',
+        discountSubtotal:12.00
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价',
+        discountSubtotal:4.00
+      }
+    ];
+    expect(result).toEqual(expected);
+  })
+});
+
+describe("calculateSavedTotal",function () {
+  it("calculateSavedTotal test",function () {
+    let itemDiscountSubtotal = [
+      {
+        id:'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        amount:1,
+        type: '指定菜品半价',
+        discountSubtotal:9.00
+
+      },
+      {
+        id:'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        amount:2,
+        type: '满30减6元',
+        discountSubtotal:12.00
+      },
+      {
+        id:'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        amount:1,
+        type: '指定菜品半价',
+        discountSubtotal:4.00
+      }
+    ];
+    let total = 38.00;
+    let result = calculateSavedTotal(itemDiscountSubtotal,total);
+    let expected = 25.00;
+    expect(result).toEqual(expected);
+  })
+});
+
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
