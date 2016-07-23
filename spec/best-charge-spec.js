70004deac2ed1327805145019e9d338e8596f690
corvoh <corvoh123@gmail.com>
2016-07-23 13:52:23 +0800

    just fix a bug

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -47,53 +47,244 @@
 //
 // });
 
-describe("splitItems", function() {
-  it('splitItems should split all the selected items', function () {
-    let selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let formattedItems = formatItems(selectedItems);
+describe("formatItems", function() {
+
+  it('["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"] should be formatted', function () {
+    let input = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     let expected = [
       {
-        barcode: "ITEM0001",
+        id: "ITEM0001",
         amount: 1
       },
       {
-        barcode: "ITEM0013",
+        id: "ITEM0013",
         amount: 2
       },
       {
-        barcode: "ITEM0022",
+        id: "ITEM0022",
         amount: 1
       }
     ];
-    expect(formattedItems).toEqual(expected)
-  });
-
-  it('should generate best charge when best is 满30减6元', function() {
-    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-满30减6元，省6元
------------------------------------
-总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
-
-  it('should generate best charge when no promotion can be used', function() {
-    let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
------------------------------------
-总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+    let output = formatItems(input);
+    expect(output).toEqual(expected);
   });
 
 });
+
+describe("subdivideItems", function () {
+
+  it("should give a subdivision to each one", function () {
+    let input = [
+      {
+        id: "ITEM0001",
+        amount: 1
+      },
+      {
+        id: "ITEM0013",
+        amount: 2
+      },
+      {
+        id: "ITEM0022",
+        amount: 1
+      }
+    ];
+    let expected = [
+      {
+        id: "ITEM0001",
+        amount: 1,
+        name: '黄焖鸡',
+        price: 18.00
+      },
+      {
+        id: "ITEM0013",
+        amount: 2,
+        name: '肉夹馍',
+        price: 6.00
+      },
+      {
+        id: "ITEM0022",
+        amount: 1,
+        name: '凉皮',
+        price: 8.00
+      }
+    ];
+    let argument = loadAllItems();
+    let output = subdivideItems(input, argument);
+    expect(output).toEqual(expected);
+  });
+
+});
+
+describe("subtotalItems", function () {
+
+  it("should count the subtotal for each one" , function () {
+    let input = [
+      {
+        id: "ITEM0001",
+        amount: 1,
+        name: '黄焖鸡',
+        price: 18.00
+      },
+      {
+        id: "ITEM0013",
+        amount: 2,
+        name: '肉夹馍',
+        price: 6.00
+      },
+      {
+        id: "ITEM0022",
+        amount: 1,
+        name: '凉皮',
+        price: 8.00
+      }
+    ];
+    let expected = [
+      {
+        id: "ITEM0001",
+        amount: 1,
+        name: '黄焖鸡',
+        price: 18.00,
+        subtotal: 18.00
+      },
+      {
+        id: "ITEM0013",
+        amount: 2,
+        name: '肉夹馍',
+        price: 6.00,
+        subtotal: 12.00
+      },
+      {
+        id: "ITEM0022",
+        amount: 1,
+        name: '凉皮',
+        price: 8.00,
+        subtotal: 8.00
+      }
+    ];
+    let output = subtotalItems(input);
+    expect(output).toEqual(expected);
+  });
+
+});
+
+// describe("addEachPromotion", function () {
+//
+//   it("should add a type to each item", function () {
+//     let input = [
+//       {
+//         id: "ITEM0001",
+//         amount: 1,
+//         name: '黄焖鸡',
+//         price: 18.00,
+//         subtotal: 18.00
+//       },
+//       {
+//         id: "ITEM0013",
+//         amount: 2,
+//         name: '肉夹馍',
+//         price: 6.00,
+//         subtotal: 12.00
+//       },
+//       {
+//         id: "ITEM0022",
+//         amount: 1,
+//         name: '凉皮',
+//         price: 8.00,
+//         subtotal: 8.00
+//       }
+//     ];
+//     let expected = [
+//       {
+//         id: "ITEM0001",
+//         amount: 1,
+//         name: '黄焖鸡',
+//         price: 18.00,
+//         subtotal: 18.00,
+//         type: "指定菜品半价"
+//       },
+//       {
+//         id: "ITEM0013",
+//         amount: 2,
+//         name: '肉夹馍',
+//         price: 6.00,
+//         subtotal: 12.00,
+//         type: "无"
+//       },
+//       {
+//         id: "ITEM0022",
+//         amount: 1,
+//         name: '凉皮',
+//         price: 8.00,
+//         subtotal: 8.00,
+//         type: "指定菜品半价"
+//       }
+//     ];
+//     let argument = loadAllItems();
+//     let output = subdivideItems(input, argument);
+//     expect(output).toEqual(expected);
+//   });
+//
+// });
+
+// describe("discountItems", function () {
+//
+//   it("should count each item`s discount", function () {
+//     let input = [
+//       {
+//         id: "ITEM0001",
+//         amount: 1,
+//         name: '黄焖鸡',
+//         price: 18.00,
+//         subtotal: 18.00,
+//         type: "指定菜品半价"
+//       },
+//       {
+//         id: "ITEM0013",
+//         amount: 2,
+//         name: '肉夹馍',
+//         price: 6.00,
+//         subtotal: 12.00,
+//         type: "无"
+//       },
+//       {
+//         id: "ITEM0022",
+//         amount: 1,
+//         name: '凉皮',
+//         price: 8.00,
+//         subtotal: 8.00,
+//         type: "指定菜品半价"
+//       }
+//     ];
+//     let expected = [
+//       {
+//         id: "ITEM0001",
+//         amount: 1,
+//         name: '黄焖鸡',
+//         price: 18.00,
+//         subtotal: 18.00,
+//         type: "指定菜品半价"
+//       },
+//       {
+//         id: "ITEM0013",
+//         amount: 2,
+//         name: '肉夹馍',
+//         price: 6.00,
+//         subtotal: 12.00,
+//         type: "无"
+//       },
+//       {
+//         id: "ITEM0022",
+//         amount: 1,
+//         name: '凉皮',
+//         price: 8.00,
+//         subtotal: 8.00,
+//         type: "指定菜品半价"
+//       }
+//     ];
+//     let output = discountItems(addedItems, allPromotions);
+//     expect(output).toEqual(expected);
+//   })
+//
+// });
+
+
--- src/best-charge.js
+++ src/best-charge.js
@@ -4,43 +4,66 @@ function bestCharge(selectedItems) {
   let allItems = loadAllItems();
   let allPromotions = loadPromotions();
   let formatedItems = formatItems(selectedItems);
-  let searchedItems = searchEachPromotion(formatedItems);
-  let subdevidedItems = subdivideItems(searchedItems, allItems);
+  let subdevidedItems = subdivideItems(formatedItems, allItems);
   let subtotaledItems = subtotalItems(subdevidedItems);
-  let discountedItems = discountItems(subtotaledItems, allPromotions);
+  let addedItems = addEachPromotion(subtotaledItems, allPromotions);
+  let discountedItems = discountItems(addedItems, allPromotions);
   let chargedItems = chargeItmes(discountedItems);
   return chargedItems;
 }
 
 function formatItems(selectedItems) {
   let formatedItems = [];
-  // let tempItems = selectedItems;
-  for(let item of selectedItems) {
+  let tempItems = selectedItems;
+  for(let item of tempItems) {
     let splitTag = item.split(" x ");
     formatedItems.push({
-      barcode: splitTag[0],
+      id: splitTag[0],
       amount: parseInt(splitTag[1]) || 1
     });
   }
   return formatedItems;
 }
 
-function searchEachPromotion(splitedItems) {
-  let searchedItems = [];
-  return searchedItems;
-}
-
-function subdivideItems(searchedItems) {
+function subdivideItems(formatedItems, allItems) {
   let subdevidedItems = [];
+  for(let one of formatedItems) {
+    for(let each of allItems) {
+      if(one["id"] === each["id"]) {
+        subdevidedItems.push(Object.assign({}, one, {name: each.name, price: each.price}));
+        break;
+      }
+    }
+  }
   return subdevidedItems;
 }
 
 function subtotalItems(subdevidedItems) {
   let subtotaledItems = [];
+  for(let item of subdevidedItems) {
+    let subtotal = item.amount * item.price;
+    subtotaledItems.push(Object.assign({}, item, {subtotal: subtotal}));
+  }
   return subtotaledItems;
 }
 
-function discountItems(subtotaledItems, allPromotions) {
+function addEachPromotion(subtotaledItems, allPromotions) {
+  let addedItems = [];
+  for(let one of subtotaledItems) {
+    for(let each of allPromotions) {
+      if(one.id === each.id) {
+        addedItems.push(Object.assign({}, one, {type: each.type}));
+        break;
+      } else {
+        addedItems.push(Object.assign({}, one, {type: "无"}));
+        break;
+      }
+    }
+  }
+  return addedItems;
+}
+
+function discountItems(addedItems, allPromotions) {
   let discountedItems = [];
   return discountedItems;
 }
