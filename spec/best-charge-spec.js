de53a6f9cf56fa3a99d7dde115b68e45c068f473
RDD4 <815389420@qq.com>
2016-07-23 15:05:27 +0800

    大教室－任丹丹

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,4 +1,56 @@
-describe('Take out food', function () {
+describe('formatItems',function () {
+  it('should split selectedItems to id and count',function () {
+    let selectedItems = ["ITEM0001 x 1"];
+    let result = formatItems(selectedItems);
+    expect(result).toEqual([{"id":"ITEM0001","count":1}]);
+  });
+});
+describe('matchAllItems',function () {
+  it('should show information of good',function () {
+    let ItemAndCount = [{"id":"ITEM0001","count":1}];
+    let result = matchAllItems(ItemAndCount,allItems);
+    expect(result).toEqual([{"id":"ITEM0001","name":"黄焖鸡","price":18,"count":1}]);
+  });
+});
+describe('calculateOriginSubtotal',function () {
+  it('should calculate originSubtotal of every good',function () {
+    let cartItems = [{"id":"ITEM0001","name":"黄焖鸡","price":18,"count":1}];
+    let result = calculateOriginSubtotal(cartItems);
+    expect(result).toEqual([{"id":"ITEM0001","name":"黄焖鸡","price":18,"count":1,"originSubtotal":18}]);
+  });
+});
+describe('calculateFirstSaving',function () {
+  it('should calculate firstSaving of goods',function () {
+    let originSubtotalList = [{"id":"ITEM0001","name":"黄焖鸡","price":18,"count":1,"originSubtotal":18}];
+    let result = calculateFirstSaving(originSubtotalList,allPromotions);
+    expect(result).toEqual(0);
+  });
+});
+describe('calculateSecondSaving',function () {
+  it('should calculate secondSaving of goods',function () {
+    let originSubtotalList = [{"id":"ITEM0001","name":"黄焖鸡","price":18,"count":1,"originSubtotal":18}];
+    let result = calculateSecondSaving(originSubtotalList,allPromotions);
+    expect(result).toEqual(9);
+  });
+});
+describe('getBestSaving',function () {
+  it('should get bestSaving of goods',function () {
+    let firstSaving = 0;
+    let secondSaving = 9;
+    let result = getBestSaving(firstSaving,secondSaving);
+    expect(result).toEqual(9);
+  });
+});
+describe('calculateTotal',function () {
+  it('should get total of goods',function () {
+    let originSubtotalList = [{"id": "ITEM0001","name": "黄焖鸡","price": 18.00,"count":1,"originSubtotal": 18.00}];
+    let bestSaving = 9;
+    let result = calculateTotal(originSubtotalList,bestSaving);
+    expect(result).toEqual(9);
+  });
+});
+
+/*describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
@@ -45,4 +97,4 @@ describe('Take out food', function () {
     expect(summary).toEqual(expected)
   });
 
-});
+});*/
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,148 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+"use strict"
+
+let selectedItems = [
+  "ITEM0001 x 1",
+  "ITEM0013 x 2",
+  "ITEM0022 x 1"
+];
+//分割
+function formatItems(selectedItems) {
+  let ItemAndCount = [];
+  for(let i=0;i<selectedItems.length;i++){
+    let temp = selectedItems[i].split(' x ');
+    ItemAndCount.push(Object.assign({},{id:temp[0]},{count:Number(temp[1])}));
+  }
+  return ItemAndCount;
 }
+/*console.log(JSON.stringify(formatItems(selectedItems)));
+let a1 = formatItems(selectedItems);*/
+//展示所有商品信息
+let allItems = loadAllItems();
+
+//匹配商品信息
+function matchAllItems(ItemAndCount,allItems) {
+  var cartItems = [];
+  for(let i=0;i<allItems.length;i++){
+    var existList = ItemAndCount.find(function (item) {
+      return item.id === allItems[i].id;
+    });
+    if(existList){
+      cartItems.push(Object.assign({},allItems[i],{count:existList.count}));
+    }
+  }
+  return cartItems;
+}
+/*console.log(JSON.stringify(matchAllItems(a1,allItems)));
+let a2 = matchAllItems(a1,allItems);*/
+
+
+//计算原始小计
+function calculateOriginSubtotal(cartItems) {
+  let originSubtotalList  =[];
+  for(let i=0;i<cartItems.length;i++){
+    var originSubtotal = parseFloat(cartItems[i].price)*cartItems[i].count;
+    originSubtotalList.push(Object.assign({},cartItems[i],{originSubtotal:originSubtotal}));
+  }
+  return originSubtotalList;
+}
+/*console.log(JSON.stringify(calculateOriginSubtotal(a2)));
+let a3 = calculateOriginSubtotal(a2);*/
+
+//展示所有优惠信息
+let allPromotions = loadPromotions();
+let tag = 0;
+//计算第一种优惠节省
+function calculateFirstSaving(originSubtotalList,allPromotions) {
+  tag = 1;
+  let firstSaving = 0;
+  let sub = 0;
+  for(let i=0;i<originSubtotalList.length;i++){
+    sub += originSubtotalList[i].originSubtotal;
+  }
+  if(sub > 30){
+    firstSaving = 6;
+  }else{
+    firstSaving = 0;
+  }
+  return firstSaving;
+}
+/*console.log(calculateFirstSaving(a3,allPromotions));
+let a4 = calculateFirstSaving(a3,allPromotions);*/
+
+//计算第二种优惠节省
+function calculateSecondSaving(originSubtotalList,allPromotions){
+  tag = 2;
+  let tempArray = [];
+  let pro = allPromotions[1].items;
+  let secondSaving = 0;
+  for(let i=0;i<originSubtotalList.length;i++){
+    let existList = pro.find(function (item) {
+      return item === originSubtotalList[i].id;
+    });
+    if(existList){
+      tempArray.push(Object.assign({},originSubtotalList[i]));
+    }
+    for(let j=0;j<tempArray.length;j++){
+      tempArray[j].originSubtotal = (tempArray[j].originSubtotal)/2;
+      secondSaving += tempArray[j].originSubtotal;
+    }
+  }
+  return secondSaving;
+}
+/*console.log(calculateSecondSaving(a3,allPromotions));
+let a5 = calculateSecondSaving(a3,allPromotions);*/
+
+//得到最优惠节省
+function getBestSaving(firstSaving,secondSaving) {
+  let bestSaving = (firstSaving > secondSaving) ? secondSaving : firstSaving;
+  if(firstSaving = secondSaving){
+    bestSaving = firstSaving;
+  }
+  return bestSaving;
+}
+/*console.log(getBestSaving(a4,a5));
+let a6 = getBestSaving(a4,a5);*/
+
+//计算总计
+function calculateTotal(originSubtotalList,bestSaving) {
+  let total = 0;
+  let st = 0;
+  for(let i=0;i<originSubtotalList.length;i++){
+    st += originSubtotalList[i].originSubtotal;
+  }
+  total = st - bestSaving;
+  return total;
+}
+/*console.log(calculateTotal(a3,a6));*/
+
+//输出
+function print(originSubtotalList,bestSaving,total) {
+  console.log("=============　订餐明细　=============");
+  for(let i=0;i<originSubtotalList.length;i++){
+    console.log(originSubtotalList[i].name+" x "+originSubtotalList[i].count
+                 +" = "+originSubtotalList[i].originSubtotal);
+  }
+  console.log("-----------------------------------");
+  if(tag === 1){
+    console.log("使用优惠：\n"+"满30减6元，省6元");
+  }
+  if(tag === 2){
+    console.log("使用优惠：\n"+"指定菜品半价"+","+"省"+ bestSaving +"(元)");
+  }
+  console.log("-----------------------------------");
+  console.log("总计"+total+"(元)");
+  console.log("===================================");
+}
+
+//主调
+function bestCharge(selectedItems) {
+  let ItemAndCount = formatItems(selectedItems);
+  let cartItems = matchAllItems(ItemAndCount,allItems);
+  let originSubtotalList = calculateOriginSubtotal(cartItems);
+  let firstSaving = calculateFirstSaving(originSubtotalList,allPromotions);
+  let secondSaving = calculateSecondSaving(originSubtotalList,allPromotions);
+  let bestSaving = getBestSaving(firstSaving,secondSaving);
+  let total = calculateTotal(originSubtotalList,bestSaving);
+  print(cartItems,bestSaving,total);
+}
+console.log(bestCharge(selectedItems));
