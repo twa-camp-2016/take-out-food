d6acc684d6d99bd25049ba09125e72c1fceab17a
Leonard LiuTao <364125168@qq.com>
2016-07-23 13:01:27 +0800

    #4

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,94 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+    return /*TODO*/;
 }
+
+//#1
+function formatInputs(inputs) {
+    //debugger;
+    return inputs.map((input) => {
+        //if(input.includes('x')) {
+        let [id, count] = input.split('x');
+        return {id: id.trim(), count: parseInt(count.trim())};
+        //}
+    });
+}
+
+//#2
+
+function _getExistentElementById(array, id) {
+    return array.find(element => element.id === id);
+}
+function buildItems(formattedInputs, allItems) {
+    //debugger;
+    return formattedInputs.map(({id,count}) => {
+        let {name,price} = _getExistentElementById(allItems, id);
+        return {id, name, price, count};
+    });
+}
+
+
+//#3
+function calculateItemsCharge(items) {
+    return items.map((item) => {
+        let itemCharge = item.count * item.price;
+        return Object.assign({}, item, {itemCharge: parseFloat(itemCharge.toFixed(0))});
+    });
+}
+
+//#4
+function choosePromotions(itemsWithCharge, promotions) {
+    let promotionOne = promotions.find((promotion) => promotion.type === '满30减6元');
+    let promotionTwo = promotions.find((promotion) => promotion.type === '指定菜品半价');
+    //return itemsWithCharge.reduce((result,item) => {
+    //    let hasPromoted = promotionTwo.items.includes(item.id);
+    //    let itemSaved = hasPromoted ? item.itemCharge / 2 : 0;
+    //    //return Object.assign({}, promotionTwo, item.name, itemSaved);
+    //    //return hasPromoted;
+    //    //return itemSaved;
+    //    result.type
+    //},{});
+
+
+    let result = [];
+    let prpmotionTwoItemSaved = 0;
+    let prpmotionOneItemSaved = 0;
+
+    let promotedItemName = [];
+    itemsWithCharge.forEach((item) => {
+
+        let hasPromoted = promotionTwo.items.includes(item.id);
+        if (hasPromoted) {
+
+            prpmotionTwoItemSaved += hasPromoted ? item.itemCharge / 2 : 0;
+            promotedItemName.push(item.name);
+
+        }
+        //result.push(itemSaved);
+    });
+    debugger;
+
+    //promotionTwo
+    let totalPrice = 0;
+    itemsWithCharge.forEach((item) => {
+        //let hasPromoted = promotionTwo.items.includes(item.id);
+        totalPrice += item.itemCharge;
+
+    });
+    if (totalPrice > 30) {
+        prpmotionOneItemSaved = (totalPrice / 30) * 6;
+    }
+
+    //choose
+    //let type;
+    let type = prpmotionTwoItemSaved > prpmotionOneItemSaved ? promotionTwo.type :promotionOne.type;
+    //
+    //let type = promotionTwo.type;
+    let saved = prpmotionTwoItemSaved;
+
+
+    return {type, promotedItemName, saved};
+
+}
+
+//Expected Object({ type: '指定菜品半价', promotedItemName: [ '黄焖鸡', '凉皮' ], prpmotionTwoItemSaved: 13 })
+//to equal Object({ type: '指定菜品半价', savedItems: [ '黄焖鸡', '凉皮' ], saved: 13 }).
