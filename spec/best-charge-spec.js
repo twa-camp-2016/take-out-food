f858b2628d624d6af4f4634dc0c525a26b2f50a3
ZT1996 <294238532@qq.com>
2016-07-27 09:45:37 +0800

    take-out-food

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,6 @@
+'use strict';
+/*global describe,beforeEach,expect,it,spyOn*/
+
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
@@ -14,7 +17,7 @@ describe('Take out food', function () {
 -----------------------------------
 总计：25元
 ===================================`.trim();
-    expect(summary).toEqual(expected)
+    expect(summary).toEqual(expected);
   });
 
   it('should generate best charge when best is 满30减6元', function() {
@@ -45,29 +48,30 @@ describe('Take out food', function () {
     expect(summary).toEqual(expected)
   });
 
-  it('should get count Items',() => {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let items = loadAllItems();
-    let countItems = getCountItems(inputs,items);
+  it('should get correct splittedItems',() => {
+    const selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    const allItems = loadAllItems();
+    const itemArray = getItemArray(selectedItems,allItems);
+
     const expectText = [
       {
-        item: {
-          id: 'ITEM0001',
+        item:{
+          id:'ITEM0001',
           name: '黄焖鸡',
           price: 18.00
         },
-        count: 1
+        count:1
       },
       {
-         item: {
-           id: 'ITEM0013',
-           name: '肉夹馍',
-           price: 6.00
-         },
+        item:{
+          id: 'ITEM0013',
+          name: '肉夹馍',
+          price: 6.00
+        },
         count:2
       },
       {
-        item: {
+        item:{
           id: 'ITEM0022',
           name: '凉皮',
           price: 8.00
@@ -75,56 +79,271 @@ describe('Take out food', function () {
         count:1
       }
     ];
-
-    expect(countItems).toEqual(expectText);
+    expect(itemArray).toEqual(expectText);
   });
 
-  it('should get correct new Items',() => {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let items = loadAllItems();
-    let countItems = getCountItems(inputs,items);
-    let promotions = loadPromotions();
-    let newItems = countPromotions(countItems,promotions);
+  it('get correct subtotal array',() => {
+    const itemArray = [
+    {
+        item:{
+          id:'ITEM0001',
+          name: '黄焖鸡',
+          price: 18.00
+        },
+        count:1
+      },
+      {
+        item:{
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00
+      },
+        count:2
+      },
+      {
+        item:{
+          id: 'ITEM0022',
+          name: '凉皮',
+          price: 8.00
+        },
+        count:1
+      }
+    ];
+    const totalArray = getSubtotalArray(itemArray);
 
     const expectText = [
       {
-        countItem: {
-          item: {
-            id: 'ITEM0001',
+        countItem:{
+          item:{
+            id:'ITEM0001',
             name: '黄焖鸡',
             price: 18.00
           },
-          count: 1
+          count:1
         },
-        subtotal:9.00,
-        save:9.00
+        subtotal:18.00
       },
       {
         countItem:{
-          item: {
+          item:{
             id: 'ITEM0013',
             name: '肉夹馍',
             price: 6.00
           },
           count:2
         },
-        subtotal:12.00,
-        save:0
+        subtotal:12.00
       },
       {
         countItem:{
-          item: {
+          item:{
             id: 'ITEM0022',
             name: '凉皮',
             price: 8.00
           },
           count:1
         },
-        subtotal:4.00,
-        save:4.00
+        subtotal:8.00
       }
     ];
 
-    expect(newItems).toEqual(expectText);
+    expect(totalArray).toEqual(expectText);
+  });
+
+  it('should get promotion one saves',() => {
+    const totalArray = [
+      {
+        countItem:{
+          item:{
+            id:'ITEM0001',
+            name: '黄焖鸡',
+            price: 18.00
+          },
+          count:1
+        },
+        subtotal:18.00
+      },
+      {
+        countItem:{
+          item:{
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+          },
+          count:2
+        },
+        subtotal:12.00
+      },
+      {
+        countItem:{
+          item:{
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+          },
+          count:1
+        },
+        subtotal:8.00
+      }
+    ];
+
+    const savesA = firstPromotion(totalArray);
+    const expectText = {total:38,saves_A:6};
+
+    expect(savesA).toEqual(expectText);
+  });
+
+  it('should get correct promotion two saves',() => {
+    const totalArray = [
+      {
+        countItem:{
+          item:{
+            id:'ITEM0001',
+            name: '黄焖鸡',
+            price: 18.00
+          },
+          count:1
+        },
+        subtotal:18.00
+      },
+      {
+        countItem:{
+          item:{
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+          },
+          count:2
+        },
+        subtotal:12.00
+      },
+      {
+        countItem:{
+          item:{
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+          },
+          count:1
+        },
+        subtotal:8.00
+      }
+    ];
+    const allPromotions = loadPromotions();
+    const savesB = secondPromotion(totalArray,allPromotions);
+    const expectText = {total:38,saves_B:13};
+
+    expect(savesB).toEqual(expectText);
+  });
+
+  it('should get correct items',() => {
+    const savesA = {total:38,saves_A:6};
+    const savesB = {total:38,saves_B:13};
+
+    const typeArray = promotionTypeItem(savesA, savesB);
+    const expectText =
+      {
+        type:'指定菜品半价',
+        saves:13
+      };
+    expect(typeArray).toEqual(expectText);
+  });
+
+  it('should get correct items',() => {
+    const savesA = {total:24,saves_A:0};
+    const savesB = {total:24,saves_B:0};
+
+    const typeArray = promotionTypeItem(savesA, savesB);
+    const expectText =
+    {
+      type:'不打折',
+      saves:0
+    };
+    expect(typeArray).toEqual(expectText);
+  });
+  it('should get correct list',() => {
+
+    const totalArray = [
+      {
+        countItem:{
+          item:{
+            id:'ITEM0001',
+            name: '黄焖鸡',
+            price: 18.00
+          },
+          count:1
+        },
+        subtotal:18.00
+      },
+      {
+        countItem:{
+          item:{
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+          },
+          count:2
+        },
+        subtotal:12.00
+      },
+      {
+        countItem:{
+          item:{
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+          },
+          count:1
+        },
+        subtotal:8.00
+      }
+    ];
+    const list = getItemList(totalArray);
+    const expectText =  `黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元`;
+
+    expect(list).toEqual(expectText);
+  });
+
+  it('should get correct promotion list',() => {
+    const typeArray =
+      {
+        type:'指定菜品半价',
+        saves:13
+      };
+    const promotionList = getPromotionList(typeArray);
+    const expectText = `使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元`;
+
+    expect(promotionList).toEqual(expectText);
+  });
+
+  it('should get correct item list',() => {
+    const list =  `黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元`;
+    const promotionList = `使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元`;
+    const typeArray =
+    {
+      type:'指定菜品半价',
+      saves:13
+    };
+    const total = 38;
+    const receipt = printList(promotionList,list,total,typeArray);
+
+    const expectText = `
+============= 订餐明细 =============
+黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元
+-----------------------------------
+使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元
+-----------------------------------
+总计：25元
+===================================`;
+    expect(receipt).toEqual(expectText);
   });
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,38 +1,137 @@
+//import _ from 'lodash';
 'use strict';
 function bestCharge(selectedItems) {
-  let items = loadAllItems();
-  let countItems = getCountItems(selectedItems,items);
-  let promotions = loadPromotions();
-  let newItems = countPromotions(countItems,promotions);
+
+  const allItems = loadAllItems();
+  const itemArray = getItemArray(selectedItems,allItems);
+  const totalArray = getSubtotalArray(itemArray);
+  const allPromotions = loadPromotions();
+  const savesA = firstPromotion(totalArray);
+  const savesB = secondPromotion(totalArray,allPromotions);
+  const typeArray = promotionTypeItem(savesA, savesB);
+  const list = getItemList(totalArray);
+  const promotionList = getPromotionList(typeArray);
+  const total = totalPrice(totalArray);
+  const receipt = printList(promotionList,list,total,typeArray);
+  return receipt;
 }
 
-function getCountItems(selectedItems,items) {
+function getItemArray(selectedItems,allItems){
+
   let countItems = [];
 
-  for(let selectedItem of selectedItems){
-    let splittedItem = selectedItem.split(' x ');
-    let id = splittedItem[0];
-    let count = parseInt(splittedItem[1]);
+  for (let selectedItem of selectedItems) {
+    let splitItem = selectedItem.split(' x ');
+    let id = splitItem[0];
+    let count = parseInt(splitItem[1]);
 
-    let countItem = countItems.find(countItem => items.id === id);
+    let countItem = countItems.find(countItem => allItems.id === id);
 
-    if(countItem){
+    if (countItem) {
       countItem.count += count;
-    }else {
-      let item = items.find(item => item.id === id);
-      countItems.push({item,count});
+    } else {
+      let item = allItems.find(item => item.id === id);
+      countItems.push({item, count});
     }
   }
   return countItems;
 }
 
-function countPromotions(countItems,promotions) {
-  
+function getSubtotalArray(itemArray) {
+
+  return itemArray.map(array => {
+    let subtotal = array.item.price * array.count;
+    return {countItem:array, subtotal};
+  });
 }
 
+function firstPromotion(totalArray) {
 
+  let total = totalPrice(totalArray);
 
+  if(total >= 30){
+    return {total,saves_A:6};
+  }else return {total,saves_A:0};
 
+}
 
+function totalPrice(totalArray) {
+  let total = 0;
+  for(let array of totalArray){
+    total += array.subtotal;
+  }
+  return total;
+}
+function secondPromotion(totalArray,allPromotions){
 
+  let total = totalPrice(totalArray);
+  let saves = 0;
+  let selectedItems = selectedPromotionItems(totalArray,allPromotions[1]);
+  for(let item of selectedItems){
+    saves += item.countItem.item.price / 2;
+  }
+  return {total,saves_B:saves};
+}
 
+function selectedPromotionItems(totalArray,promotion) {
+  let items = [];
+  totalArray.map(array => promotion.items.find(item =>{
+    if(item === array.countItem.item.id){
+      items.push(array);
+    }
+  }));
+  return items;
+}
+
+function promotionTypeItem(savesA, savesB) {
+  if(savesA.saves_A >= savesB.saves_B){
+    if(savesA.saves_A === 0){
+      return {type:'不打折',saves:savesA.saves_A};
+    }else {
+      return {type: '满30减6元', saves: savesA.saves_A};
+    }
+  }else {
+    return {type:'指定菜品半价', saves:savesB.saves_B};
+  }
+}
+
+function getItemList(totalArray) {
+
+  return totalArray.map(array => {
+   return `${array.countItem.item.name} x ${array.countItem.count} = ${array.subtotal}元`;})
+    .join('\n');
+}
+
+function getPromotionList(typeArray) {
+
+    if(typeArray.type === '指定菜品半价'){
+      return `使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省${typeArray.saves}元`;
+    }
+    else if(typeArray.type === '满30减6元'){
+      return `使用优惠:
+满30减6元，省6元`;
+    }
+    else return;
+}
+
+function printList(promotionList,list,total,typeArray) {
+  let realTotal = total - typeArray.saves;
+  if(typeArray.type === '不打折'){
+    return `
+============= 订餐明细 =============
+${list}
+-----------------------------------
+总计：${total}元
+===================================`;
+  }
+  else {
+  return `
+============= 订餐明细 =============
+${list}
+-----------------------------------
+${promotionList}
+-----------------------------------
+总计：${realTotal}元
+===================================`;}
+}
